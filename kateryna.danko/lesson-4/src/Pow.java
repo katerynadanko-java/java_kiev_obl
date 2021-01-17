505f408e9f29aea88037177d45f0bf4801917b5e
katerynadanko21 <katerynadanko21@gmail.com>
2021-01-17 13:38:58 +0200

    in commond line

--- kateryna.danko/lesson-4/src/Pow.java
+++ kateryna.danko/lesson-4/src/Pow.java
@@ -1,6 +1,7 @@
 
 public class Pow {
     public static void main(String[] args) {
+
         System.out.println("You enter the first number: "+ args[0]);
         int integerNumber1 = Integer.parseInt(args[0]);
         System.out.println("You enter the second number: "+ args[1]);
@@ -8,10 +9,14 @@ public class Pow {
         System.out.println("You enter the third number: "+ args[2]);
         int integerNumber3 = Integer.parseInt(args[2]);
 
+        int pow = (int)Math.pow(integerNumber1, integerNumber2);
+        String stringPow = Integer.toString(pow);
+
+        int integerPowInNumberSystem  = Integer.parseInt(stringPow, integerNumber3);
+
+
         System.out.println("Raised the first number to the power of the second number " +
                 "and deduced in the " +
-                "number system entered by the third number: " +
-                Integer.parseInt(Integer.toString((int)Math.pow(integerNumber1,
-                        integerNumber2)), integerNumber3));
+                "number system entered by the third number: " + integerPowInNumberSystem);
     }
 }
